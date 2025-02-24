5d46e8e45c072aec3fab47f5ba38225174a4c51b
JenniferDaleffi <jenniferdaleffii@gmail.com>
2025-02-24 20:02:47 -0300

    first commit

---
+++ finance-walk-api/finance-walk-api/src/main/java/br/com/br/fiap/finance_walk_api/FinanceWalkApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.br.fiap.finance_walk_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceWalkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceWalkApiApplication.class, args);
+	}
+
+}
